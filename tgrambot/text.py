27650e98f21d6f72173f6b3bd05533f2ff8bbf07
Restyled.io <commits@restyled.io>
2022-10-03 16:47:01 +0000

    Restyled by reorder-python-imports

--- tgrambot/text.py
+++ tgrambot/text.py
@@ -1,7 +1,20 @@
-from telegram_text import (Bold, Chain, Code, Hashtag, InlineCode, InlineUser,
-                           Italic, Link, OrderedList, PlainText, Spoiler,
-                           Strikethrough, Text, TOMLSection, Underline,
-                           UnorderedList, User)
+from telegram_text import Bold
+from telegram_text import Chain
+from telegram_text import Code
+from telegram_text import Hashtag
+from telegram_text import InlineCode
+from telegram_text import InlineUser
+from telegram_text import Italic
+from telegram_text import Link
+from telegram_text import OrderedList
+from telegram_text import PlainText
+from telegram_text import Spoiler
+from telegram_text import Strikethrough
+from telegram_text import Text
+from telegram_text import TOMLSection
+from telegram_text import Underline
+from telegram_text import UnorderedList
+from telegram_text import User
 from telegram_text.bases import Element
 
 __all__ = [
